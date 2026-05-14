8501d6a161f18c2baf2942a993af81c48cd1541f
kaisuzuk <szkakg@gmail.com>
2026-05-14 00:43:53 +0000

    epoll

--- src/ft_ircd.cpp
+++ src/ft_ircd.cpp
@@ -15,6 +15,7 @@
 #include <cstring>
 #include <unistd.h>
 
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
@@ -43,16 +44,57 @@ int main(int argc, char *argv[])
     listen(server_fd, 10);
     std::cout << "Listening on port " << port << std::endl;
 
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+    int epfd = epoll_create1(0);
 
-    std::cout << "client connected!" << std::endl;
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = server_fd;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &ev);
 
-    const char *msg = "hello\r\n";
-    send(client_fd, msg, strlen(msg), 0);
+    struct epoll_event events[64];
 
-    close(client_fd);
+    while (true)
+    {
+        int nfds = epoll_wait(epfd, events, 64, -1);
+        
+        for (int i = 0; i < nfds; ++i)
+        {
+            int fd = events[i].data.fd;
+
+            if (fd == server_fd)
+            {
+                struct sockaddr_in client_addr;
+                socklen_t client_len = sizeof(client_addr);
+                int client_fd = accept4(server_fd, (struct sockaddr *)&client_addr, &client_len, SOCK_NONBLOCK);
+
+                ev.events = EPOLLIN;
+                ev.data.fd = client_fd;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev);
+
+                std::cout << "client connected: fd = " << client_fd << std::endl;
+
+                const char *msg = "hello\r\n";
+                send(client_fd, msg, strlen(msg), 0);
+            }
+            else
+            {
+                char buf[512];
+                int n = recv(fd, buf, sizeof(buf) - 1, 0);
+                if (n <= 0)
+                {
+                    std::cout << "client disconnected: fd = " << fd << std::endl;
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+                    close(fd);
+                }
+                else
+                {
+                    buf[n] = '\0';
+                    std::cout << "fd = " << fd << "says: " << buf << std::endl;
+                }
+            }
+        }
+    }
     close(server_fd);
+    close(epfd);
     return (EXIT_SUCCESS);
 }
