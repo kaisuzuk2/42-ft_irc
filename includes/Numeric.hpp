6e25bb3eeb24a1d3c9a16e5a4dd040f9886f7b1c
kaisuzuk <szkakg@gmail.com>
2026-05-21 01:55:22 +0000

    numeric.hpp作成

---
+++ includes/Numeric.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Numeric.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-05-21 01:45:23 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-05-21 01:45:23 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifncef NUMERIC_HPP
+# define NUMERIC_HPP
+
+enum Numeric 
+{
+    RPL_WELCOME             = 1, 
+    RPL_YOURHOST            = 2,
+    RPL_CREATED             = 3,
+    RPL_MYINFO              = 4,
+
+    ERR_UNKNOWNCOMMAND      = 421,
+    ERR_NONICKNAMEGIVEN     = 431,
+    ERR_ERRONEUSNICKNAME    = 432,
+    ERR_NICKNAMEINUSE       = 433,
+    ERR_NOTREGISTERED       = 451,
+    ERR_NEEDMOREPARAMS      = 461,
+    ERR_ALREADYREGISTERED   = 462,
+    ERR_PASSWDMISMATCH      = 464,
+}
+
+#endif
