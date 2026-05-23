ca528974af2c0d68fc32ec6869a4c4c09c233222
kaisuzuk <szkakg@gmail.com>
2026-05-23 23:36:35 +0000

    channelクラス作成

---
+++ includes/Channel.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-05-23 23:23:24 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-05-23 23:23:24 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+class Client;
+
+class Channel
+{
+    private:
+        std::string                 _name;
+        std::string                 _topic;
+        std::string                 _topicSetBy;
+        time_t                      _topicSetAt;
+        std::map<Client *, bool>    _members;
+        unsigned int                _modes;
+        std::string                 _key;
+        unsigned int                _limit;
+
+    public:
+        Channel();
+        ~Channel();
+
+        const std::string           &_geName() const;
+        const std::string           &_getTopic() const;
+
+        void                        addMember(Client *client, bool isOper = false);
+        void                        removeMember(Client *client);
+        bool                        hasMember(Client *client) const;
+        bool                        isOper(Client *client) const;
+        
+};
+
+#endif
