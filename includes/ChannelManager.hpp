d2a95493896e811e0004fa3d9c84eb56a7f4ed86
kaisuzuk <szkakg@gmail.com>
2026-05-24 00:34:38 +0000

    channelmanagerクラス作成

---
+++ includes/ChannelManager.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ChannelManager.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-05-24 00:13:56 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-05-24 00:13:56 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHANNELMANAGER_HPP
+# define CHANNELMANAGER_HPP
+
+#include <string>
+#include <map>
+
+class Channel;
+
+class ChannelManager 
+{
+    private:
+        std::map<std::string, Channel *>    _channels;
+
+    public:
+        ChannelManager();
+        ~ChannelManager();
+
+        Channel                             *_find(const std::string &name) const;
+        Channel                             *_create(const std::string &name);
+        void                                _remove(const std::string &name);
+};
+
+#endif
