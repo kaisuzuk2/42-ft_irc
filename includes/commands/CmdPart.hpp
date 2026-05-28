7526f8d87fde4585a26fbb0e3885d822d05b85b8
kaisuzuk <szkakg@gmail.com>
2026-05-28 00:32:17 +0000

    partコマンドクラス作成

---
+++ includes/commands/CmdPart.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CmdPart.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-05-28 00:30:29 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-05-28 00:30:29 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CMDPART_HPP
+# define CMDPART_HPP
+
+#include "ACommand.hpp"
+
+class CmdPart: public ACommand 
+{
+    public:
+        CmdPart();
+        ~CmdPart();
+
+        void    _execute(FtIRCd &serverInstance, Client &client, const std::vector<std::string> &params);
+};
+
+#endif
