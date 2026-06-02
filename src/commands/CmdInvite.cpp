da48a81cba664a4dbeba870158e5f760f0c6af18
kaisuzuk <szkakg@gmail.com>
2026-06-02 23:27:46 +0000

    invite登録済みユーザのみ

--- src/commands/CmdInvite.cpp
+++ src/commands/CmdInvite.cpp
@@ -80,7 +80,7 @@ void CmdInvite::_execute(FtIRCd &serverInstance, Client &client, const std::vect
         return ;
     }
 
-    target = serverInstance._getClients()._findByNick(nick);
+    target = serverInstance._getClients()._findByNick(nick, true);
     if (!target)
     {
         client._writeNumeric(ERR_NOSUCHNICK, serverInstance._getServername(), nick + " :No such nick");
