57d047b505903ce2b9a42c8586b74bdf6b217b9b
kaisuzuk <szkakg@gmail.com>
2026-06-10 01:27:43 +0000

    不要なコメント削除

--- includes/ft_ircd.hpp
+++ includes/ft_ircd.hpp
@@ -64,7 +64,7 @@ class FtIRCd
         static const int            kArgIdxPassword = 2;
 
 
-    public: // ### TODO: const版いるかどうか考えよう
+    public: 
         const std::string           &_getServername() const;
         ClientManager               &_getClients();
         const ClientManager         &_getClients() const;
