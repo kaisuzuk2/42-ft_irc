cde0c2a6afc96b5ce33ea982862c16e0cd190ba4
kaisuzuk <szkakg@gmail.com>
2026-06-11 01:49:32 +0000

    nosuchchannel実装

---
+++ includes/NumericHelper.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   NumericHelper.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-06-11 01:40:51 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-06-11 01:40:51 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NUMERICHELPER_HPP
+# define NUMERICHELPER_HPP
+
+#include "NumericReply.hpp"
+#include "Numeric.hpp"
+
+namespace Numerics
+{
+    class NoSuchChannel;
+}
+
+class Numerics::NoSuchChannel: public NumericReply
+{
+    public:
+    NoSuchChannel(const std::string &cname);
+}
+
+#endif
