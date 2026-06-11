b6f68e5617d7095336decbddf6b5411ca8d533f9
kaisuzuk <szkakg@gmail.com>
2026-06-11 01:36:56 +0000

    数値リプライ用クラス作成

---
+++ includes/NumericReply.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   NumericReply.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-06-11 01:33:18 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-06-11 01:33:18 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NUMERICREPLY_HPP
+# define NUMERICREPLY_HPP
+
+class NumericReply 
+{
+    private:
+        unsigned int        _num;
+        std::string         _msg;
+
+    public:
+        NumericReply(unsigned int num, const std::string &msg);
+
+        unsigned int        _getNum() const;
+        const std::string   &_getMsg() const;
+};
+
+#endif
