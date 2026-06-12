a02f5dda70a41c10756c7f1d5b6d3b7766f743a5
kaisuzuk <szkakg@gmail.com>
2026-06-12 02:58:42 +0000

    cmdlistクラス定義

---
+++ includes/commands/CmdList.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CmdList.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-06-12 02:56:55 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-06-12 02:56:55 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CMDLIST_HPP
+# define CMDLIST_HPP
+
+#include "ft_ircd.hpp"
+#include "ACommand.hpp"
+
+class CmdList: public ACommand
+{
+    public:
+        CmdList();
+        ~CmdList();
+
+        void    _execute(FtIRCd &serverInstance, Client &client, const std::vector<std::string> &params);
+};
+
+#endif
