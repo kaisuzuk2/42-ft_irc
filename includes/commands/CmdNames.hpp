4921cbe3093e6ecfc8f26d19d5648686f5bcfdc5
kaisuzuk <szkakg@gmail.com>
2026-06-13 00:55:09 +0000

    cmdnames定義

---
+++ includes/commands/CmdNames.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CmdNames.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-06-13 00:53:36 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2026-06-13 00:53:36 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CMDNAMES_HPP
+# define CMDNAMES_HPP
+
+#include "ACommand.hpp"
+
+class CmdNames: public ACommand
+{
+    public:
+        CmdNames();
+        ~CmdNames();
+
+        void    _execute(FtIRCd &serverInstance, Client &client, const std::vector<std::string> &params);
+};
+
+#endif
